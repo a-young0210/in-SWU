6fe6e447fd6fd9e408a5f62e945c53fbdf0f830d
AYOUNG YANG <dkdudab@hanmail.net>
2022-04-11 21:37:38 +0900

    Add files via upload

---
+++ WindowsProgramming/6장 사용자 인터페이스 - 메뉴만들기/Simple1/ChildView.cpp
@@ -0,0 +1,101 @@
+﻿
+// ChildView.cpp: CChildView 클래스의 구현
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "Simple1.h"
+#include "ChildView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CChildView
+
+CChildView::CChildView()
+{
+	m_color = RGB(255, 0, 0);
+}
+
+CChildView::~CChildView()
+{
+}
+
+
+BEGIN_MESSAGE_MAP(CChildView, CWnd)
+	ON_WM_PAINT()
+	ON_COMMAND(ID_COLOR_RED, &CChildView::OnColorRed)
+	ON_COMMAND(ID_COLOR_GREEN, &CChildView::OnColorGreen)
+	ON_COMMAND(ID_COLOR_BLUE, &CChildView::OnColorBlue)
+END_MESSAGE_MAP()
+
+
+
+// CChildView 메시지 처리기
+
+BOOL CChildView::PreCreateWindow(CREATESTRUCT& cs) 
+{
+	if (!CWnd::PreCreateWindow(cs))
+		return FALSE;
+
+	cs.dwExStyle |= WS_EX_CLIENTEDGE;
+	cs.style &= ~WS_BORDER;
+	cs.lpszClass = AfxRegisterWndClass(CS_HREDRAW|CS_VREDRAW|CS_DBLCLKS, 
+		::LoadCursor(nullptr, IDC_ARROW), reinterpret_cast<HBRUSH>(COLOR_WINDOW+1), nullptr);
+
+	return TRUE;
+}
+
+void CChildView::OnPaint() 
+{
+	CPaintDC dc(this); // 그리기를 위한 디바이스 컨텍스트입니다.
+	
+	// TODO: 여기에 메시지 처리기 코드를 추가합니다.
+	// 
+	//사각형 그리기
+	CBrush brush(m_color);
+	dc.SelectStockObject(BLACK_PEN);
+	dc.SelectObject(&brush);
+	dc.Rectangle(50, 50, 200, 200);
+
+	///타원 그리기
+	CPen pen;
+	pen.CreatePen(PS_SOLID, 5, m_color);
+	dc.SelectObject(&pen);
+	dc.SelectStockObject(NULL_BRUSH);
+	dc.Ellipse(250, 50, 400, 200);
+	CRect rect = { 250, 50, 400, 200 };
+	CString str = _T("타원");
+	dc.SetTextColor(m_color);
+	dc.DrawText(str, &rect, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
+	
+	// 그리기 메시지에 대해서는 CWnd::OnPaint()를 호출하지 마십시오.
+}
+
+
+
+void CChildView::OnColorRed()
+{
+	// TODO: 여기에 명령 처리기 코드를 추가합니다.
+	m_color = RGB(255, 0, 0);
+	Invalidate();
+
+}
+
+
+void CChildView::OnColorGreen()
+{
+	// TODO: 여기에 명령 처리기 코드를 추가합니다.
+	m_color = RGB(0, 255, 0);
+	Invalidate();
+}
+
+
+void CChildView::OnColorBlue()
+{
+	// TODO: 여기에 명령 처리기 코드를 추가합니다.
+	m_color = RGB(0, 0, 255);
+	Invalidate();
+}
